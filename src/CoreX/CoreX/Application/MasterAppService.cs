bb90fb7bc380132adfdd28b5fb662358ec737b3a
Bishoy Labib <bishoymamdouh@hotmail.com>
2018-04-27 17:24:12 +0200

    still trying custom swagger

---
+++ src/CoreX/CoreX/Application/MasterAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Application;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+
+namespace CoreX.Application
+{
+    public class MasterAppService<TEntity, TEntityDto> : AsyncCrudAppService<TEntity, TEntityDto> 
+        where TEntity : class, IEntity<int>
+        where TEntityDto : IEntityDto<int>
+    {
+        public MasterAppService(IRepository<TEntity> repository) : base(repository)
+        {
+            
+        }
+    }
+}
---
+++ src/CoreX/CoreX/Emit/Swagger/SwaggerGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreX.Models;
+
+namespace CoreX.Emit.Swagger
+{
+    public class SwaggerGenerator : ICodeGenerator
+    {
+        public void GenerateCode(Model model)
+        {
+            var path = "Generated\\swagger.json";
+            var root = new Rootobject
+            {
+                swagger = "2.0",
+                basePath = "/corex/api/",
+                info = new Info
+                {
+                    title = "CoreX API Swagger",
+                    version = "1.0.0"
+                },
+                schemes = new string[] { "http", "https" },
+                tags = new List<Tag1>()
+            };
+
+        }
+    }
+}
